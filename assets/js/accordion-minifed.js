43f82a7e568e47f2a3140a4ca301e908e281ab9f
wojtasss298 <69736855+wojtasss298@users.noreply.github.com>
2021-04-23 09:08:03 +0200

    files from cdn

--- assets/js/accordion-minifed.js
+++
@@ -1,2 +0,0 @@
-var acc=document.getElementsByClassName("accordion");var i;for(i=0;i<acc.length;i++){acc[i].addEventListener("click",function(){var panel=this.nextElementSibling;if(panel.style.maxHeight){panel.style.maxHeight=null}else{let active=document.querySelectorAll(".accordion-div .accordion.active");for(let j=0;j<active.length;j++){active[j].classList.remove("active");active[j].nextElementSibling.style.maxHeight=null}
-this.classList.toggle("active");panel.style.maxHeight=panel.scrollHeight+"px"}})}
