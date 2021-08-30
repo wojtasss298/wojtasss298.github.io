bb0f8083687c454ebb9ee751b5cf9a86f24faaa8
wojtasss298 <69736855+wojtasss298@users.noreply.github.com>
2021-08-30 14:14:27 +0200

    add audio

---
+++ ham/assets/js/player.js
@@ -0,0 +1,11 @@
+function playerplay(){
+    document.getElementById('player').play();
+}
+
+function playerstop(){
+    document.getElementById('player').pause();
+}
+
+function playermute(){
+    document.getElementById('player').muted=!document.getElementById('player').muted;
+}
